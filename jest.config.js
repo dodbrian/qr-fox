fef27182e6fef067dca4826d48a0cf2dafa41773
Denis Zimin <roydronov@gmail.com>
2026-03-30 22:11:21 +0200

    test: migrate to vitest from jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>/__tests__"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/build/__tests__/setup.js"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!scripts/**"],
-};
